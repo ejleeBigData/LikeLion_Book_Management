5c96667b9db6eaa854b628d4c673d5951ff31fd1
이은정 <ej2grateful@gmail.com>
2025-07-10 15:48:47 +0900

    REST API - 완

---
+++ src/main/java/com/leeej/book_management/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.leeej.book_management.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "hello";
+    }
+
+    @PostMapping("/post")
+    public String sayPostHello() {
+        return "post hello";
+    }
+
+    @PutMapping("/put")
+    public String sayPutHello() {
+        return "put hello";
+    }
+}
